4431aa991c57f7aa61f05cfed8934a54f41c8503
bertinma <bertin_ma@icloud.com>
2023-02-21 02:11:34 +0100

    initial commit training

--- game.py
+++ game.py
@@ -2,6 +2,22 @@ import numpy as np
 import random 
 
 DIRECTIONS = ['right', 'down', 'left', 'up']
+DIRECTIONS_LETTERS = ['d', 's', 'q', 'z']
+
+
+def compute_distance(apple, snake_part):
+    """
+    Computes the distance between the apple and the head of the snake.
+
+    Arguments:
+    apple -- a tuple of the form (x, y) representing the position of the apple
+    snake -- a list of tuples of the form (x, y) representing the position of the snake
+
+    Returns:
+    A float representing the distance between the apple and the head of the snake
+    """
+    return np.sqrt((apple[0] - snake_part[0])**2 + (apple[1] - snake_part[1])**2)
+
 
 def to_categorical(labels, num_classes=4):
     """
@@ -25,10 +41,10 @@ def to_categorical(labels, num_classes=4):
 class Game():
     def __init__(self, size):
         self.size = size
-        self.create_plate()
         self.score = 0
         self.game_over = False
         self.direction = -1
+        self.create_plate()
 
     def new_apple(self):
         while True:
@@ -41,6 +57,18 @@ class Game():
         self.plate[self.apple[0], self.apple[1]] = 2
         for i in self.snake:
             self.plate[i[0], i[1]] = 1
+
+    def display_plate(self):
+        # replace 0 by ' ' and 1 by 'X' and 2 by 'O'
+        plate = np.where(self.plate == 0, '.', self.plate)
+        plate = np.where(plate == '1.0', 'X', plate)
+        plate = np.where(plate == '2.0', 'O', plate)
+        if not any(v == -1 or v == self.size for v in self.snake[0]):
+            plate[self.snake[0][0], self.snake[0][1]] = 'x'
+        for i in range(plate.shape[0]):
+            print(' '.join(plate[i]))
+            # print('\n')
+
 
     def create_plate(self):
         self.plate = np.zeros((self.size, self.size))
@@ -70,10 +98,18 @@ class Game():
 
     def step(self):
         # 0 - right, 1 - down, 2 - left, 3 - up
+        
         while True:
-            new_direction = random.randint(0, 3)
+            # new_direction = random.randint(0, 3)
+            in_str = input("Enter direction: ")
+            if in_str not in DIRECTIONS_LETTERS:
+                print("Wrong direction")
+                continue
+            new_direction = DIRECTIONS_LETTERS.index(in_str)
             # if (self.direction == 0 and new_direction != 2) or (self.direction == 1 and new_direction != 3) or (self.direction == 2 and new_direction != 0) or (self.direction == 3 and new_direction != 1):
             #     break
+            if self.direction == new_direction:
+                break
             if not (new_direction + self.direction) % 2 == 0:
                 self.direction = new_direction
                 break
@@ -92,7 +128,7 @@ class Game():
         
         # check if game over
         # if new_head[0] < 0 or new_head[0] >= self.size or new_head[1] < 0 or new_head[1] >= self.size or new_head in self.snake:
-        if any(v == 0 or v == self.size for v in new_head) or new_head in self.snake:
+        if any(v == -1 or v == self.size for v in new_head) or new_head in self.snake:
             self.game_over = True
             return
         # check if apple eaten
@@ -107,42 +143,137 @@ class Game():
 
         self.draw_plate()
 
-    def build_model_input(self):
-        input = []
-        for block in self.snake:
-            input.append(block[0] / self.size)
-            input.append(block[1] / self.size)
-        for _ in range(47 - len(self.snake)):
-            input.append(0)
-            input.append(0)
-        input.append(self.apple[0] / self.size)
-        input.append(self.apple[1] / self.size)
-        input.extend(to_categorical([self.direction])[0])
+    # def build_model_input(self):
+    #     input = []
+    #     for block in self.snake:
+    #         input.append(block[0] / self.size)
+    #         input.append(block[1] / self.size)
+    #     for _ in range(47 - len(self.snake)):
+    #         input.append(0)
+    #         input.append(0)
+    #     input.append(self.apple[0] / self.size)
+    #     input.append(self.apple[1] / self.size)
+    #     input.extend(to_categorical([self.direction])[0])
         
-        assert len(input) == 100, "Input length is not 100"
-        return input
+    #     assert len(input) == 100, "Input length is not 100"
+    #     return input
+
+    def get_state(self):
+        """
+        Return the state.
+        The state is a numpy array of 11 values, representing:
+            - Danger 1 OR 2 steps ahead
+            - Danger 1 OR 2 steps on the right
+            - Danger 1 OR 2 steps on the left
+            - Snake is moving left
+            - Snake is moving right
+            - Snake is moving up
+            - Snake is moving down
+            - The food is on the left
+            - The food is on the right
+            - The food is on the upper side
+            - The food is on the lower side      
+        """
+
+        state = [
+            # Danger straight
+            (self.direction == 0 and ((self.snake[0][1] == self.size - 1) or any([self.snake[0] == (s[0], s[1] - 1) for s in self.snake[3:]]))) or
+            (self.direction == 1 and ((self.snake[0][0] == self.size - 1) or any([self.snake[0] == (s[0] - 1, s[1]) for s in self.snake[3:]]))) or
+            (self.direction == 2 and ((self.snake[0][1] == 0) or any([self.snake[0] == (s[0], s[1] + 1) for s in self.snake[3:]]))) or  
+            (self.direction == 3 and ((self.snake[0][0] == 0) or any([self.snake[0] == (s[0] + 1, s[1]) for s in self.snake[3:]]))),
+
+            # Danger right
+            (self.direction == 0 and ((self.snake[0][0] == self.size - 1) or any([self.snake[0] == (s[0] - 1, s[1]) for s in self.snake[3:]]))) or
+            (self.direction == 1 and ((self.snake[0][1] == 0) or any([self.snake[0] == (s[0], s[1] + 1) for s in self.snake[3:]]))) or
+            (self.direction == 2 and ((self.snake[0][0] == 0) or any([self.snake[0] == (s[0] + 1, s[1]) for s in self.snake[3:]]))) or
+            (self.direction == 3 and ((self.snake[0][1] == self.size - 1) or any([self.snake[0] == (s[0], s[1] - 1) for s in self.snake[3:]]))),
+            # Danger left
+            (self.direction == 0 and ((self.snake[0][0] == 0) or any([self.snake[0] == (s[0] + 1, s[1]) for s in self.snake[3:]]))) or
+            (self.direction == 1 and ((self.snake[0][1] == self.size - 1) or any([self.snake[0] == (s[0], s[1] - 1) for s in self.snake[3:]]))) or
+            (self.direction == 2 and ((self.snake[0][0] == self.size - 1) or any([self.snake[0] == (s[0] - 1, s[1]) for s in self.snake[3:]]))) or
+            (self.direction == 3 and ((self.snake[0][1] == 0) or any([self.snake[0] == (s[0], s[1] + 1) for s in self.snake[3:]]))),
+
+            # (self.direction == 0 and self.snake[0][0] >= self.size - 2) or
+            # (self.direction == 1 and self.snake[0][1] <= 1) or
+            # (self.direction == 2 and self.snake[0][0] <= 1) or
+            # (self.direction == 3 and self.snake[0][1] >= self.size - 2), 
+            # # Danger left
+            # (self.direction == 0 and self.snake[0][0] <= 1) or
+            # (self.direction == 1 and self.snake[0][1] >= self.size - 2) or
+            # (self.direction == 2 and self.snake[0][0] >= self.size - 2) or
+            # (self.direction == 3 and self.snake[0][1] <= 1),
+            # Move direction
+            self.direction == 0,
+            self.direction == 1,
+            self.direction == 2,
+            self.direction == 3,
+            # Food location
+            self.apple[0] < self.snake[0][0],  # food up   
+            self.apple[0] > self.snake[0][0],  # food down
+            self.apple[1] < self.snake[0][1],  # food left
+            self.apple[1] > self.snake[0][1]  # food right
+        ]
+        return np.array(state, dtype=int)
+
+    def get_reward(self):
+        if self.game_over:
+            return -10
+        elif self.snake[0] == self.apple:
+            return 10
+        else:
+            if compute_distance(self.snake[0], self.apple) < compute_distance(self.snake[1], self.apple):
+                return 1
+            else:
+                return -1
+
+    def compute_next_state(self, new_direction):
+        # if not (new_direction + self.direction) % 2 == 0:
+        self.direction = new_direction
+        # move
+        if self.direction == 0:
+            new_head = (self.snake[0][0] , self.snake[0][1] + 1)
+        elif self.direction == 1:
+            new_head = (self.snake[0][0] + 1, self.snake[0][1])
+        elif self.direction == 2:
+            new_head = (self.snake[0][0], self.snake[0][1] - 1)
+        elif self.direction == 3:
+            new_head = (self.snake[0][0] - 1, self.snake[0][1])
+
+        
+        # check if game over
+        # if new_head[0] < 0 or new_head[0] >= self.size or new_head[1] < 0 or new_head[1] >= self.size or new_head in self.snake:
+        if any(v == -1 or v == self.size for v in new_head) or new_head in self.snake:
+            self.game_over = True
+        # check if apple eaten
+        if new_head == self.apple:
+            self.score += 1
+            self.new_apple()
+            self.snake.append(self.snake[-1])
+        
+        for i in range(len(self.snake)-1, 0, -1):
+            self.snake[i] = self.snake[i-1]
+        self.snake[0] = new_head
+        if not self.game_over:
+            self.draw_plate()
+        return self.get_state()
+
+    def is_terminal(self):
+        return self.game_over
+
 
 if __name__ == '__main__':
     game = Game(10)
-    print(game.plate)
-
-    print(game.apple)
-    print(game.snake)
-
-    print(game.score)
-    print(game.game_over)
-    print(game.direction)
 
     while not game.game_over:
-        game.step()
-        print(game.plate)
-        print(game.apple)
-        print(game.snake)
-        print(game.score)
-        print(game.game_over)
-        print(DIRECTIONS[game.direction])
+        game.display_plate()
+        print("Apple position : ", game.apple)
+        print("Snake position : ", game.snake)
+        print("Score : ", game.score)
+        print("Game over : ", game.game_over)
+        print("Direction : ", DIRECTIONS[game.direction])
         input_model = game.build_model_input()
-        print(input_model)
+        print("Model input : ", input_model)
         print('\n\n')
+        game.step()
 
 
---
+++ train.py
@@ -0,0 +1,166 @@
+import model 
+from game import Game
+import torch
+from tqdm import tqdm
+import torch.nn.functional as F
+import random
+
+myModel = model.Model().float()
+
+EPOCHS = 200000
+GAMMA = 0.9
+directions_letters = ['d', 's', 'q', 'z']
+direction_str = ['right', 'down', 'left', 'up']
+optimizer = torch.optim.Adam(myModel.parameters(), lr=0.0005)
+memory = []
+results = []
+
+
+def get_direction_with_action(action, direction):
+    # go straight
+    if action == 0:
+        return direction
+    else:
+        # initial direction is right 
+        if direction == 0:
+            # left 
+            if action == 1:
+                return 3
+            # right
+            elif action == 2:
+                return 1
+        # initial direction is down
+        elif direction == 1:
+            # left 
+            if action == 1:
+                return 0
+            # right
+            elif action == 2:
+                return 2
+        # initial direction is left
+        elif direction == 2:
+            # left
+            if action == 1:
+                return 1
+            # right
+            elif action == 2:
+                return 3
+        # initial direction is up
+        elif direction == 3:
+            # left 
+            if action == 1:
+                return 2
+            # right
+            elif action == 2:
+                return 0
+
+def plot_results(results):
+    import matplotlib.pyplot as plt
+    import numpy as np
+    """Plot the results of the training
+    Each row is a game, each value in a row is a step and have the same abscissa (step)
+
+    Plot the mean of the results by row in red
+    Plot the max of the results by in blue
+    Plot each point in black, with small point size.
+    """ 
+    means = [(i, np.mean(result)) for i, result in enumerate(results)]
+    maxs = [(i, np.max(result)) for i, result in enumerate(results)]
+    plt.plot([i for i, _ in means], [result for _, result in means], c='red')
+    plt.plot([i for i, _ in maxs], [result for _, result in maxs], c='blue')
+    for i, result in enumerate(results):
+        for r in result:
+            plt.scatter(i, r, c='black', s=1)
+    plt.show()
+
+for epoch in tqdm(range(EPOCHS)):
+    if epoch > 0:
+        print(game.game_over, steps)
+    game = Game(10)
+    steps = 0
+    tmp = []
+    while not game.game_over and steps < 100:
+        old_state = game.get_state()
+        # print("Model input : ", old_state)
+        old_state_tensor = torch.tensor(old_state, dtype=torch.float32, requires_grad=True)
+
+        with torch.no_grad():
+            q_values = myModel(old_state_tensor)
+        # game.step(output)
+        
+        action = torch.argmax(q_values)
+        curent_q_value = q_values[action]
+        
+        direction = get_direction_with_action(action, game.direction)
+        print("Game direction : ", game.direction)
+        print("Action : ", action)
+        print("Direction : ", direction_str[direction])
+        next_state = game.compute_next_state(direction)
+        reward = game.get_reward()
+        if reward > 0:
+            print("Score : ", game.score)
+            steps = 0
+        elif reward < -2:
+            print("/!\ Game Over /!\ ")
+
+        if game.is_terminal():
+            observed_q_value = reward
+        else:
+            new_state = torch.tensor(next_state, dtype=torch.float32)
+            next_q_value = torch.max(myModel(new_state))
+            observed_q_value = reward + (GAMMA * next_q_value)
+
+        myModel.train()
+        torch.set_grad_enabled(True)
+        target_f = q_values.clone()
+        target_f.detach()
+        target_f[action] = observed_q_value
+        # print("Target : ", target_f)
+        # print("Q Values : ", q_values)
+        optimizer.zero_grad()
+        loss = F.mse_loss(q_values, target_f).requires_grad_(True)
+        if reward < 0:
+            print("Loss : ", loss)
+        # print("Loss : ", loss)
+        loss.backward()
+        optimizer.step()
+        tmp.append(reward)
+
+        # memory 
+        steps += 1
+        memory.append((old_state, action, reward, next_state, game.is_terminal()))
+        game.display_plate()
+        if next_state[0] == 1:
+            print('Forward danger')
+        if next_state[1] == 1:
+            print('Right danger')
+        if next_state[2] == 1:
+            print('Left danger')
+        print("\n\n")
+
+    results.append(tmp)
+
+
+    if len(memory) > 1000:
+        memory = random.sample(memory, 100)
+    for state, action, reward, next_state, done in memory:
+            myModel.train()
+            torch.set_grad_enabled(True)
+            target = reward
+            # print(next_state, state)
+            next_state_tensor = torch.tensor(next_state, dtype=torch.float32)
+            state_tensor = torch.tensor(state, dtype=torch.float32, requires_grad=True)
+            if not done:
+                target = reward + GAMMA * torch.max(myModel(next_state_tensor))
+            output = myModel(state_tensor)
+            target_f = output.clone()
+            target_f[action] = target
+            target_f.detach()
+            optimizer.zero_grad()
+            loss = F.mse_loss(output, target_f).requires_grad_(True)
+            loss.backward()
+            optimizer.step()
+# save model 
+torch.save(myModel.state_dict(), "model.pt")
+print(results)
+# plot_results(results)
